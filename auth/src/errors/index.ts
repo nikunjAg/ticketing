0ad337290f108646dd56b8f31741db66b64dcbf3
Nikunj Aggarwal <aggarwalnikunj1999@gmail.com>
2022-09-08 11:55:03 +0530

    Implemented signin, currentuser and signout routes and added validate-request, currentuser and require auth middlewares and not-authorized custom error class

--- auth/src/errors/index.ts
+++ auth/src/errors/index.ts
@@ -4,3 +4,4 @@ export { RequestValidationError } from './request-validation-error';
 export { DatabaseConnectionError } from './database-connection-error';
 export { NotFoundError } from './not-found-error';
 export { BadRequestError } from './bad-request-error';
+export { NotAuthorizedError } from './not-authorized-error';
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 
-import { RequestValidationError, BadRequestError } from '../errors';
+import { BadRequestError } from '../errors';
 import { User } from '../models';
+import { validateRequest } from '../middlewares';
 
 const router = express.Router();
 
@@ -18,17 +19,12 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password length must be between 4 and 20 charachters')
   ],
+  validateRequest,
   async (
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
   ) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
