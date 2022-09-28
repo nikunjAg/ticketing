cc492c6660c53eb13a1372bafab7a5eab68b1492
Nikunj Aggarwal <aggarwalnikunj1999@gmail.com>
2022-09-28 17:34:04 +0530

    Added CRUD for tickets service

---
+++ tickets/src/routes/show.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response, } from 'express';
+import { NotFoundError } from '@nagticketing/common';
+
+import { Ticket } from '../model';
+
+const router = express.Router();
+
+router.get(
+  '/api/tickets/:id',
+  async (req: Request, res: Response) => {
+    const { id: ticketId } = req.params;
+    
+    // Return all the tickets of no ticketId is present
+    if (!ticketId) {
+      const tickets = await Ticket.find({});
+      
+      return res
+			.status(200)
+			.json({ message: "Tickets fetched successfully", tickets });
+    }
+
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    res
+      .status(200)
+      .json({
+        message: 'Ticket found sucessfully',
+        ticket
+      });
+  }
+);
+
+export {router as showTicketRouter};
